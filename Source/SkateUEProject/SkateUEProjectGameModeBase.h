30eb605f98872e62129cf187b9fe14012c2262dc
Dany Falk <dany_falk@hotmail.com>
2024-07-06 16:50:59 -0300

    setting up project

---
+++ Source/SkateUEProject/SkateUEProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkateUEProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKATEUEPROJECT_API ASkateUEProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
